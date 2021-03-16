b3a26041f2c0abebed1c00f4c137a4e825e29b52
Isaac Walters <waltersi@my.lanecc.edu>
2021-03-15 19:04:21 -0700

    Ready for turn in Azure stuff incomming

---
+++ WCountry/WCountry/Repos/FakeRepo.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WCountry.Models;
+
+namespace WCountry.Repos
+{
+    public class FakeRepo : IWShops
+    {
+        public List<WShop> wshops = new List<WShop>();
+        public IQueryable<WShop> WShop { get { return (IQueryable<WShop>)wshops; } }
+
+        public void AddItem(Item item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int AddReviews(List<Review> reviews)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddWShop(WShop wshop)
+        {
+            wshops.Add(wshop);
+        }
+
+        public List<Item> GetAllItems()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<WShop> GetAllWShops()
+        {
+            return wshops;
+        }
+
+        public WShop GetWShopByName(string name)
+        {
+            return (from r in wshops where r.WShopName == name select r).FirstOrDefault<WShop>();
+        }
+
+        public WShop GetWShopByOwner(string name)
+        {
+            return (from r in wshops where r.Owner.Name == name select r).FirstOrDefault<WShop>();
+        }
+
+        public void UpdateWShop(WShop wshop)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
